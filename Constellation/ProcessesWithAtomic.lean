6e600b040f6491afb8e636e337dbd19b0d64d9b9
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2026-07-23 17:03:11 +0200

    whee

--- Constellation/ProcessesWithAtomic.lean
+++ Constellation/ProcessesWithAtomic.lean
@@ -4,20 +4,28 @@ import Constellation.FunRel
 import Std
 open Std
 
+/-
+Let's try an experiment.
+
+Let's ignore rules entirely for now.
+Let's just focus on the matter of atomicity.
+-/
+
 abbrev Var := String
 
-mutual
+inductive Process: Type where
+  | halt
+  | add (v: Var)
+  | rem (v: Var)
+  | par (p1 p2: Process)
+  | seq (p1 p2: Process)
+  -- | ite (v: Var) (t e: Process)
+deriving BEq
 
-  inductive Process: Type where
-    | halt
-    | add (v: Var)
-    | rem (v: Var)
-    | par (p1 p2: Process)
-    | seq (p1 p2: Process)
-    | ite (v: Var) (t e: Process)
-  deriving BEq
 
-end
+infix:85 "▸" => Process.seq
+notation:max "▪" => Process.halt
+infix:85 "∣∣" => Process.par
 
 abbrev Holds := Finset Var
 
@@ -26,35 +34,23 @@ abbrev Pending := List Process
 mutual
   inductive Exec: EndoRel (Pending × Holds) where
     | halt P H:
-        Process.halt ∈ P →
-        Exec (P, H) (P.erase Process.halt, H)
+        Exec (▪ :: P, H) (P, H)
 
     | add P H v:
         Process.add v ∈ P →
-        Exec (P, H) (P.erase (Process.add v), insert v H)
+        Exec (Process.add v :: P, H) (P, insert v H)
 
     | rem P H v:
-        Process.rem v ∈ P →
-        Exec (P, H) (P.erase (Process.rem v), H.erase v)
+        Exec (Process.rem v :: P, H) (P, H.erase v)
 
     | par P H p1 p2:
-        p1.par p2 ∈ P →
-        Exec (P, H) (p1 :: p2 :: P.erase (p1.par p2), H)
+        p1 ∣∣ p2 ∈ P →
+        Exec (p1 ∣∣ p2 :: P, H) (p1 :: p2 :: P, H)
 
     | seq P H H' p1 p2:
-        p1.seq p2 ∈ P →
         Completes ([p1], H) ([], H') →
-        Exec      (P, H) (p2 :: P.erase (p1.seq p2), H')
+        Exec      (p1 ▸ p2 :: P, H) (p2 :: P, H')
 
-    | ite_t P H v t e:
-        Process.ite v t e ∈ P →
-        v ∈ H →
-        Exec (P, H) (t :: P.erase (Process.ite v t e), H)
-
-    | ite_e P H v t e:
-        Process.ite v t e ∈ P →
-        v ∉ H →
-        Exec (P, H) (e :: P.erase (Process.ite v t e), H)
 
   inductive Completes: EndoRel (Pending × Holds) where
     | intro P H H':
@@ -77,3 +73,50 @@ theorem empty_completes (H: Holds):
   exists ([], H)
   constructor
   constructor
+
+namespace Process
+  abbrev later  (p: Process): Process := ▪ ▸ p
+  abbrev atomic (p: Process): Process := p ▸ ▪
+
+end Process
+
+theorem just_halt_completes: ∀ H, Completes ([▪], H) ([], H)
+:= by
+  intro _
+  repeat constructor
+
+
+theorem p_iff_p_later:
+  ∀ (P P': Pending) (H H': Holds) (p: Process),
+    Completes (p :: P, H) (P', H')
+    ↔
+    Completes (p.later :: P, H) (P', H')
+:= by
+  intro P P' H H' p
+  constructor
+  . intro h
+    cases h
+    constructor
+    rename_i h
+    cases h
+    rename_i h
+    constructor
+    cases h
+    . rename_i h
+      cases h
+      . unfold Process.later
+        have q := just_halt_completes H
+        have r := Exec.seq [] _ _ _ ▪ q
+
+        right
+        . sorry
+        . sorry
+        . sorry
+      . sorry
+      . sorry
+    . sorry
+  . intro h
+    cases h
+    rename_i h
+    constructor
+    sorry
